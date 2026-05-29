c3bfdc15d611ef70920f77bac5395ae9ad096ea7
Kevin Gomes <kevin.gomes.dev@gmail.com>
2026-05-28 21:28:35 -0400

    Solution

---
+++ index.js
@@ -0,0 +1,12 @@
+let code1;
+code1 = 7 + 3;
+let code2 = 200 / 4 - 10;
+let code3 = 79 % 40;
+
+let message = "The vault has been secured. The combination is: ";
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
